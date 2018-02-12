ed01035c3d1d339c68c33ec02af760e9ef20ffa8
k0t3n <k0t3n.mail@gmail.com>
2018-02-12 23:44:54 +0300

    Rename src to backend

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from src.api import urls as api_urls
+from api import urls as api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
