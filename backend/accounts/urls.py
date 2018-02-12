5cd93a0ad1cfb58f7dc901054272159fb59e2f7f
k0t3n <k0t3n.mail@gmail.com>
2018-02-13 00:30:30 +0300

    Add token authorization

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,14 +1,16 @@
-from django.conf.urls import url, include
+from django.urls import path, include
+
 from rest_framework import routers
+from rest_framework.authtoken import views as token_views
+
 from accounts import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('auth', token_views.obtain_auth_token),
+    path('', include(router.urls)),
 ]
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
 
     # Django rest framework
     'rest_framework',
+    'rest_framework.authtoken',
 
     # Project apps
     'accounts',
@@ -36,6 +37,18 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'core.urls'
+
+# REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12
+}
 
 TEMPLATES = [
     {
