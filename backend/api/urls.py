b2ff59d1694ebfc241c0d4dc253362dcc435601d
k0t3n <k0t3n.mail@gmail.com>
2018-02-14 03:42:30 +0300

    Add stats app to settings, add urls file for stats

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from accounts import urls as accounts_urls
 
 urlpatterns = [
-    path('accounts/', include(accounts_urls)),
+    path('accounts/', include('accounts.urls')),
+    path('stats/', include('stats.urls')),
 ]
