e11b0167162eb9e0e851a10433a696444a24e6f3
k0t3n <k0t3n.mail@gmail.com>
2018-02-15 18:33:37 +0300

    Release .csv file upload & parse

--- backend/stats/tasks.py
+++ backend/stats/tasks.py
@@ -1,8 +1,12 @@
-from __future__ import absolute_import, unicode_literals
+import os
+import csv
 
 from celery import Task
 from core.celery import app
 from celery.result import AsyncResult
+
+from core.settings import BASE_DIR, UPLOAD_DIR
+from .models import StatsUploadEvent, Stats, CurrencyPair
 
 
 class ParseStatsTask(Task):
@@ -19,5 +23,36 @@ class ParseStatsTask(Task):
 
 @app.task(base=ParseStatsTask)
 def start_parse(filename):
+    with open(os.path.join(BASE_DIR, UPLOAD_DIR, filename), 'r') as file_obj:
+        file_data = csv.DictReader(file_obj, delimiter=',')
+
+        data = []
+
+        for item in file_data:
+            data.append(
+                Stats(
+                    type=item['Type'].lower(),
+                    currency=CurrencyPair.objects.get_or_create(
+                        first_currency=item['Market'].split('/')[0],
+                        last_currency=item['Market'].split('/')[1],
+                    )[0],
+                    datetime=item['Date'],
+                    price=float(item['Price']),
+                    amount=float(item['Amount']),
+                    total=float(item['Total']),
+                    fee=float(item['Fee'][:-1]),
+                    order=int(item['Order Number']),
+                    base_total_less_fee=float(item['Base Total Less Fee']),
+                    quote_total_less_fee=float(item['Quote Total Less Fee']),
+                )
+            )
+
+        Stats.objects.bulk_create(data)
+
+        # print(data)
+
+    result = {
+        'uploaded_records': len(data)
+    }
 
     return
--- backend/stats/views.py
+++ backend/stats/views.py
@@ -1,9 +1,13 @@
+import os
+import csv
+
 from rest_framework import generics
 from rest_framework.parsers import MultiPartParser
 from rest_framework.views import APIView, Response
 from rest_framework.permissions import IsAuthenticated
 
 from .utils import handle_uploaded_file
+from .tasks import start_parse
 from .models import StatsUploadEvent
 from .serializers import StatsUploadEventSerializer
 
@@ -13,14 +17,18 @@ class StatsUploadView(APIView):
     Загрузка файла
     """
 
-    # TODO
     parser_classes = (MultiPartParser,)
 
     def put(self, request, filename, format=None):
         file = request.FILES['file']
         handle_uploaded_file(file, filename)  # сохраняем файл
 
+        task = start_parse.delay(filename)
 
+        StatsUploadEvent.objects.create(
+            task_id=task,
+            filename=filename,
+        )
 
         return Response(status=204)
 
