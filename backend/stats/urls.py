3f253d8ddf1a9d7ce9045e471ed628b90d5b21e2
k0t3n <k0t3n.mail@gmail.com>
2018-02-18 18:40:54 +0300

    Remove AllCurrenciesView Add permission_classes to models

--- backend/stats/urls.py
+++ backend/stats/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, re_path
+
 from .views import *
 
 urlpatterns = [
@@ -6,5 +7,4 @@ urlpatterns = [
     path('getStats', StatsView.as_view(), name='get-stats'),
     path('getUploadEvents', StatsUploadEventView.as_view(), name='stats-upload-events'),
     path('getCurrencyPairs', CurrencyPairsView.as_view(), name='get-currency-pairs'),
-    path('getAllCurrencies', AllCurrenciesView.as_view(), name='all-currencies'),
 ]
--- backend/stats/views.py
+++ backend/stats/views.py
@@ -1,13 +1,14 @@
 from rest_framework import generics
-from rest_framework.parsers import MultiPartParser, JSONParser
+from rest_framework.exceptions import ParseError
+from rest_framework.parsers import MultiPartParser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView, Response
-from rest_framework.permissions import IsAuthenticated
 
-from .utils import handle_uploaded_file
-from .tasks import start_parse
 from .models import Stats, StatsUploadEvent, CurrencyPair
 from .serializers import StatsUploadEventSerializer, CurrencyPairSerializer, StatsSerializer
+from .tasks import start_parse
+from .utils import handle_uploaded_file
 
 
 class StatsUploadView(APIView):
@@ -17,7 +18,7 @@ class StatsUploadView(APIView):
 
     parser_classes = (MultiPartParser,)
 
-    # permission_classes = IsAuthenticated
+    permission_classes = IsAuthenticated
 
     def put(self, request, filename, format=None):
         file = request.FILES['file']
@@ -58,36 +59,21 @@ class CurrencyPairsView(generics.ListAPIView):
     serializer_class = CurrencyPairSerializer
 
 
-class AllCurrenciesView(APIView):
-    """
-    Вывод всех уникальных монеток
-    """
-
-    # permission_classes = (IsAuthenticated,)
-
-    def get(self, request):
-        data = []
-        currency_pairs = CurrencyPair.objects.all()
-        for pair in currency_pairs:
-            data.append(pair.first_currency)
-            data.append(pair.last_currency)
-
-        data = set(data)
-
-        return Response(data)
-
-
-class StatsView(APIView):
+class StatsView(generics.GenericAPIView):
     """
     Вывод статистики по заданным параметрам
     """
 
     renderer_classes = (JSONRenderer,)
 
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def post(self, request, format=None):
         received_data = request.data
+
+        if received_data.get('first_currency') is None or received_data.get('lasr_currency') is None:
+            raise ParseError('Пропущен один из параметров')
+
         stats_objects = Stats.objects.filter(
             currency_pair=CurrencyPair.objects.get(
                 first_currency=received_data['first_currency'],
