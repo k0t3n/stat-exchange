5d8f60576aafef5fe2cb2433134e22b0d75acbed
k0t3n <k0t3n.mail@gmail.com>
2018-02-18 23:18:35 +0300

    Refactor serializers

--- backend/stats/serializers.py
+++ backend/stats/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from .fields import CurrencyPairField
 from .models import StatsUploadEvent, CurrencyPair, Stats
 
@@ -15,37 +16,9 @@ class CurrencyPairSerializer(serializers.ModelSerializer):
         fields = ('id', 'first_currency', 'last_currency')
 
 
-class StatsSerializer(serializers.Serializer):
-    type = serializers.CharField(
-        max_length=5,
-    )
-
+class GetStatsSerializer(serializers.ModelSerializer):
     currency_pair = CurrencyPairField()
 
-    datetime = serializers.DateTimeField()
-
-    price = serializers.DecimalField(
-        max_digits=17, decimal_places=8,  # <1 milliard
-    )
-
-    amount = serializers.DecimalField(
-        max_digits=17, decimal_places=8,  # <1 milliard
-    )
-
-    total = serializers.DecimalField(
-        max_digits=17, decimal_places=8,  # <1 milliard
-    )
-
-    fee = serializers.DecimalField(
-        max_digits=5, decimal_places=2,  # <100
-    )
-
-    order = serializers.IntegerField()
-
-    base_total_less_fee = serializers.DecimalField(
-        max_digits=17, decimal_places=8,  # <1 milliard
-    )
-
-    quote_total_less_fee = serializers.DecimalField(
-        max_digits=17, decimal_places=8,  # <1 milliard
-    )
+    class Meta:
+        model = Stats
+        fields = '__all__'
