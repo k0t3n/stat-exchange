0d4c0d3b67a3ae4636817431e31e98dc499afb5d
Zelofff <zelofflab@gmail.com>
2018-02-20 18:22:56 +0300

    Edit ListOfStatuses

--- frontend/src/components/ListOfStatuses.js
+++ frontend/src/components/ListOfStatuses.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import '../styles/ListOfStatuses.css';
 import PropTypes from 'prop-types';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { ListGroup, Badge, Label, Well } from 'react-bootstrap';
 
 const ListOfStatuses =  ({ statuses, ...props }) => {
     return (
-        <ListGroup>
-            {statuses.map(task => (
-                <ListGroupItem
-                    key={task.id}
-                    className={`status-${task.status}`}
-                >
-                    {`${task.status} Загружено: ${task.uploaded_records}`}
-                </ListGroupItem>
-            ))}
+        <ListGroup {...props}>
+            {statuses.map(task => {
+                const bsStyle = task.status === 'in_progress' ? 'warning' : task.status === 'failed' ? 'danger' : task.status;
+                const status = task.status === 'in_progress' ? 'Загружается' : task.status === 'failed' ? 'Ошибка' : 'Успешно';
+                const date = new Date(task.uploaded_at).toLocaleString();
+
+                return (
+                    <div
+                        key={task.id}
+                    >
+                        <Label bsStyle={bsStyle}>{status}</Label>
+                        <Well >
+                            Записей загружено: {task.uploaded_records}
+                            <Badge pullRight>{date}</Badge>
+                        </Well>
+                    </div>
+                )
+            })}
         </ListGroup>
     )
 };
