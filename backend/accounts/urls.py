34663f6fcf276392dd0a40a566f76895e3a0a1d9
k0t3n <k0t3n.mail@gmail.com>
2018-02-22 11:39:08 +0300

    Delete useless stuff in core/urls

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,18 +1,8 @@
 from django.urls import path, include
-
-from rest_framework import routers
 from rest_framework_jwt.views import refresh_jwt_token
 
-from accounts import views
-
-# router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
-
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('registration/', include('rest_auth.registration.urls')),
     path('refresh-token/', refresh_jwt_token),
     path('', include('rest_auth.urls')),
-    # path('', include(router.urls)),
 ]
